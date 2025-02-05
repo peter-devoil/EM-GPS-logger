d30f6037230e9eb4ab36d38d4992cbf1afefc3b3
peter de voil <peter.devoil@gmail.com>
2025-02-05 16:43:57 +1000

    bbox of all data

--- Screener/barebones/map.js
+++ Screener/barebones/map.js
@@ -29,12 +29,25 @@
               // scale factor based on the size of the bounding box and the map
               // size.
               var p1 = d3.geoPath().projection(d3.geoMercator().scale(1));
-              var bbox_path = [];
+              var bbox_path1 = null;
               if (plotData != null) {
-                   bbox_path = p1.bounds(plotData); // fixme include em data too
+                     bbox_path1 = p1.bounds(plotData);
+              }
+              var bbox_path2 = null;
+              if (emData != null) {
+                     bbox_path2 = p1.bounds(emData);
               } 
+              if (bbox_path1 == null && bbox_path2 == null) { return; }
+              var bbox_path = bbox_path1;
+              if (bbox_path == null && bbox_path2 != null) {
+                     bbox_path = bbox_path2;
+              } else if (bbox_path != null && bbox_path2 != null) {
+                     bbox_path = [[Math.min(bbox_path[0][0], bbox_path2[0][0], bbox_path[1][0], bbox_path2[1][0]),
+                                   Math.min(bbox_path[0][1], bbox_path2[0][1], bbox_path[1][1], bbox_path2[1][1])],
+                                  [Math.max(bbox_path[0][0], bbox_path2[0][0], bbox_path[1][0], bbox_path2[1][0]),
+                                   Math.max(bbox_path[0][1], bbox_path2[0][1], bbox_path[1][1], bbox_path2[1][1])]]
+              }
 
-              if (bbox_path.length == 0) { return; }
               var scale = 0.95 / Math.max(
                      (bbox_path[1][0] - bbox_path[0][0]) / width,
                      (bbox_path[1][1] - bbox_path[0][1]) / height
@@ -42,14 +55,31 @@
 
               // Get the bounding box of the features (in map units!) and use it
               // to calculate the center of the features.
-              var bbox_feature = d3.geoBounds(plotData);
-              var center = [(bbox_feature[1][0] + bbox_feature[0][0]) / 2,
-                            (bbox_feature[1][1] + bbox_feature[0][1]) / 2];
+              var bbox_feature1 = null;
+              if (plotData != null) {
+                     bbox_feature1 = d3.geoBounds(plotData);
+              }
+              var bbox_feature2 = null;
+              if (emData != null) {
+                     bbox_feature2 = d3.geoBounds(emData);
+              } 
+              if (bbox_feature1 == null && bbox_feature2 == null) { return; }
+              var bbox_feature = bbox_feature1;
+              if (bbox_feature == null && bbox_feature2 != null) {
+                     bbox_feature = bbox_feature2;
+              } else if (bbox_feature != null && bbox_feature2 != null) {
+                     bbox_feature = [[Math.min(bbox_feature[0][0], bbox_feature2[0][0], bbox_feature[1][0], bbox_feature2[1][0]),
+                                      Math.min(bbox_feature[0][1], bbox_feature2[0][1], bbox_feature[1][1], bbox_feature2[1][1])],
+                                     [Math.max(bbox_feature[0][0], bbox_feature2[0][0], bbox_feature[1][0], bbox_feature2[1][0]),
+                                      Math.max(bbox_feature[0][1], bbox_feature2[0][1], bbox_feature[1][1], bbox_feature2[1][1])]]
+              }
 
+              var center = [(bbox_feature[1][0] + bbox_feature[0][0]) / 2,
+              (bbox_feature[1][1] + bbox_feature[0][1]) / 2];
+              
               var projection = d3.geoMercator().scale(scale)
                             .center(center)
                             .translate([width / 2, height / 2]);
-
               var path = d3.geoPath(projection);
 
               var svg = d3.select(element)
